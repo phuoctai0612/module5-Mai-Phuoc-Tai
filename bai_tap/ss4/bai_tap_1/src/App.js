600336997ef19c9ba1e255fab1d9fa6e784ad67e
phuoctai0612 <sieuem2002@gmail.com>
2023-07-20 15:32:43 +0700

    baitap

--- bai_tap/ss4/bai_tap_1/src/App.js
+++ bai_tap/ss4/bai_tap_1/src/App.js
@@ -1,22 +1,26 @@
 import React,{useState} from 'react';
-
 import './App.css';
 function App() {
 const[count,setCount]=useState(0);
 const[count1,setCount1]=useState(0);
+const useIncrement=(addAmount)=>{
+
+  setCount(result=>result+addAmount);
+}
+const useIncrement1=(addAmount)=>{
+  setCount1(result=>result+addAmount);
+}
+
   return (
-
-
     <>
     <p>Count1: {count}</p>
-     <button onClick={()=>{
-      setCount((previousCount)=>previousCount+1);
-     }}>Add1</button><br/>
+     <button onClick={
+     useIncrement.bind(null,1)
+     }>Add1</button><br/>
     <p>Count2: {count1}</p>
-     <button onClick={()=>{
-      setCount1((previousCount)=>previousCount+2);
-     }}>Add2</button>
-   
+     <button onClick={
+      useIncrement1.bind(null,2)
+     }>Add2</button> 
 </>
   );
 }
